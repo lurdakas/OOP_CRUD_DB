75632cae1b6e8c6f40ca9d71e5270ca4714a5479
lurdakas <lkasileviciute@protonmail.com>
2024-06-03 14:14:44 +0300

    So far - "findById","selectAll", "create", "update" - works without errors.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.models.Author;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -7,32 +9,32 @@ import java.sql.ResultSet;
 
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Connection con = connect();
-        String query = "SELECT * FROM authors";
-        try {
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while ((rs.next())){
-                System.out.println(rs.getLong("id") + " " + rs.getString("name") + " " + rs.getString("surname"));
+        System.out.println(Author.selectAll());
+        Author a = Author.findById(69);
+        System.out.println(a);
+        a.setName("Test");
+        a.setSurname("Testaitis");
+        a.update();
+        a = Author.findById(69);
+        System.out.println(a);
+        Author.create("Testas2", "Testaitis2");
+
+    }
+
+        public static Connection connect(){
+            Connection connection = null;
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "");
+            } catch (Exception e) {
+                System.out.println("Failed to connect to database");
             }
-        }catch (Exception e){
-            System.out.println("kaboom!");
+            return connection;
         }
+
+
 
 
     }
 
-    public static Connection connect() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");
-        }catch (Exception e){
-            System.out.println("Failed to connect to database");
-        }
-
-        return connection;
-    }
-
-}
